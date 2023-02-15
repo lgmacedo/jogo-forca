94930cda0c93210d05e511a950b30ed914b58840
lgmacedo <lucasguerra.macedo@gmail.com>
2023-02-15 11:21:38 -0300

    css and image assets are now imported through the src folder

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,8 +1,17 @@
+import forca0 from "../assets/forca0.png"
+import forca1 from "../assets/forca1.png"
+import forca2 from "../assets/forca2.png"
+import forca3 from "../assets/forca3.png"
+import forca4 from "../assets/forca4.png"
+import forca5 from "../assets/forca5.png"
+import forca6 from "../assets/forca6.png"
+
+const imagensForca = [forca0, forca1, forca2, forca3, forca4, forca5, forca6]
+
 export default function Jogo(props){
-    const imagemForca = `/assets/forca${props.numErros}.png`
     return(
         <div className="Jogo">
-            <img src={imagemForca} alt="forca"/>
+            <img src={imagensForca[props.numErros]} alt="forca"/>
             <div>
                 <button>Escolher palavra</button>
                 <p>{props.palavra}</p>
