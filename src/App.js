318d41dcf6327498841936ab668234c1c192ae66
lgmacedo <lucasguerra.macedo@gmail.com>
2023-02-27 13:00:39 -0300

    code smell fix

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import palavras from "./palavras";
 import Chute from "./components/Chute";
 
 let palavraFinal = "";
+const numeroTotalLetras = 26;
 
 export default function App() {
   const [numErros, setNumErros] = useState(0);
   const [palavraMostrada, setPalavraMostrada] = useState("");
-  const [letrasAtivadas, setLetrasAtivadas] = useState(Array(26).fill(0));
+  const [letrasAtivadas, setLetrasAtivadas] = useState(Array(numeroTotalLetras).fill(0));
   const [corPalavra, setCorPalavra] = useState("");
   const [chuteDesabilitado, setChuteDesabilitado] = useState(true);
   const maxErros = 6;
@@ -20,7 +21,7 @@ export default function App() {
       palavraFinal = "";
       setNumErros(0);
       setPalavraMostrada("");
-      setLetrasAtivadas(Array(26).fill(0));
+      setLetrasAtivadas(Array(numeroTotalLetras).fill(0));
       setCorPalavra("");
     }
     palavraFinal = palavras[Math.floor(Math.random() * palavras.length)];
@@ -40,7 +41,7 @@ export default function App() {
   }
 
   function ativaLetras() {
-    setLetrasAtivadas(Array(26).fill(1));
+    setLetrasAtivadas(Array(numeroTotalLetras).fill(1));
   }
 
   function clicouLetra(letra, index) {
@@ -112,14 +113,14 @@ export default function App() {
 
   function ganhouJogo() {
     setCorPalavra("corPalavraVerde");
-    setLetrasAtivadas(Array(26).fill(0));
+    setLetrasAtivadas(Array(numeroTotalLetras).fill(0));
     setChuteDesabilitado(true);
   }
 
   function perdeuJogo() {
     setPalavraMostrada(palavraFinal);
     setCorPalavra("corPalavraVermelha");
-    setLetrasAtivadas(Array(26).fill(0));
+    setLetrasAtivadas(Array(numeroTotalLetras).fill(0));
     setChuteDesabilitado(true);
   }
 
